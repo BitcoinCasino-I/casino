cab494af4684b3f8d32c8f092637e8b4352690f8
Thanos <atha.ar@arcor.de>
2020-12-08 18:57:11 +0100

    half merge

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/main.html"))
+		tmpl := template.Must(template.ParseFiles("templates/master.html"))
 		tmpl.Execute(w, nil)
 	})
 
