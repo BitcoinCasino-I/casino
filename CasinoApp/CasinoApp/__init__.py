9bbb5d34619a05da58defaed4a2eb6202101f950
Lars <lartschey@googlemail.com>
2020-12-30 13:29:31 +0100

    Update __init__.py

--- CasinoApp/CasinoApp/__init__.py
+++ CasinoApp/CasinoApp/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # CODE IST TEILWEISE REDUNDANT UND KANN AUCH ALLGEMEIN NOCH OPTIMIERT WERDEN. FOKUS LAG ERSTMAL AUF FUNKTIONALITÄT.
-# webhook test
+# webhook test x
 
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flaskext.mysql import MySQL
