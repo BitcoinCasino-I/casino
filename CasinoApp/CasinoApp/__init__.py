5bb5ff54a11fad580afbba2d9e1afd17cf241974
Lars <lartschey@googlemail.com>
2021-01-02 16:28:23 +0100

    reset hardness level

--- CasinoApp/CasinoApp/__init__.py
+++ CasinoApp/CasinoApp/__init__.py
@@ -633,7 +633,7 @@ def slotmachine():
 							#Game stuff
 							newbalance = balance - intamounttobet
 							win = None
-							maxint = 4
+							maxint = 7
 
 							lineone = [randint(0,maxint), randint(0,maxint), randint(0,maxint)]
 							linetwo = [randint(0,maxint), randint(0,maxint), randint(0,maxint)]
