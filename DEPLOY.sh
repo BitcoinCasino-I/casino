6889c12a64116eb4b08f68901b2edb3ebc2fc0af
Lars <lartschey@googlemail.com>
2021-01-12 19:19:18 +0100

    Update DEPLOY.sh

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -177,7 +177,7 @@ sudo -H -u "$APPUSER" bash -c "sudo rm -rf /var/lib/phpmyadmin >/dev/null 2>&1";
 sudo -H -u "$APPUSER" bash -c "sudo rm -rf /usr/share/phpmyadmin >/dev/null 2>&1";
 sudo -H -u "$APPUSER" bash -c "sudo rm -rf /etc/apache2 >/dev/null 2>&1";
 sudo -H -u "$APPUSER" bash -c "sudo rm -rf /etc/mysql >/dev/null 2>&1";
-sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq purge git ufw openssh-server apache2 libapache2-mod-php7.3 libsodium23 php php-common php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline psmisc php7.3-mbstring php7.3-zip php7.3-gd php7.3-xml php7.3-curl php7.3-mysql mariadb-server mariadb-client mysql-common curl python3.7 python3-dev python3-pip python3-venv python3.7-venv libapache2-mod-wsgi-py3 libapache2-mod-security2 libmariadb-dev-compat libmariadb-dev >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq purge ufw apache2 libapache2-mod-php7.3 libsodium23 php php-common php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline psmisc php7.3-mbstring php7.3-zip php7.3-gd php7.3-xml php7.3-curl php7.3-mysql mariadb-server mariadb-client mysql-common curl python3.7 python3-dev python3-pip python3-venv python3.7-venv libapache2-mod-wsgi-py3 libapache2-mod-security2 libmariadb-dev-compat libmariadb-dev >/dev/null 2>&1";
 echo "${green}Fertig.${reset}"
 echo ""
 
