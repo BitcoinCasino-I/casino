4ef2c08de572d37838da6aa57c4ef4ccf0eef776
Lars <lartschey@googlemail.com>
2021-01-12 18:55:32 +0100

    Fix purging / resets

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -170,15 +170,20 @@ echo ""
 sudo -H -u "$APPUSER" bash -c "(echo '$APPUSERPW' | sudo -Si >/dev/null 2>&1);"
 
 # Diverse Programme / Ordner /Datenbanken zurücksetzen
-sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq purge apache2 ufw mariadb-server mariadb-client >/dev/null 2>&1";
-sudo -H -u "$APPUSER" bash -c "sudo rm -r /var/lib/mysql/* >/dev/null 2>&1";
-sudo -H -u "$APPUSER" bash -c "sudo rm -r /var/www/html/* >/dev/null 2>&1";
-sudo -H -u "$APPUSER" bash -c "sudo rm -r /var/lib/phpmyadmin >/dev/null 2>&1";
-sudo -H -u "$APPUSER" bash -c "sudo rm -r /usr/share/phpmyadmin >/dev/null 2>&1";
+echo "${yellow}Setze Programme zurück...${reset}"
+sudo -H -u "$APPUSER" bash -c "sudo rm -rf /var/lib/mysql/* >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo rm -rf /var/www/html >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo rm -rf /var/lib/phpmyadmin >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo rm -rf /usr/share/phpmyadmin >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo rm -rf /etc/apache2 >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo rm -rf /etc/mysql >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq purge git ufw openssh-server apache2 libapache2-mod-php7.3 libsodium23 php php-common php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline psmisc php7.3-mbstring php7.3-zip php7.3-gd php7.3-xml php7.3-curl php7.3-mysql mariadb-server mariadb-client mysql-common curl python3.7 python3-dev python3-pip python3-venv python3.7-venv libapache2-mod-wsgi-py3 libapache2-mod-security2 libmariadb-dev-compat libmariadb-dev >/dev/null 2>&1";
+echo "${green}Fertig.${reset}"
+echo ""
 
 # Alle notwendigen Systempakete installieren
 echo "${yellow}Installiere alle nötigen Systempakete...${reset}";
-sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq install git ufw openssh-server apache2 libapache2-mod-php7.3 libsodium23 php php-common php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline psmisc php7.3-mbstring php7.3-zip php7.3-gd php7.3-xml php7.3-curl php7.3-mysql mariadb-server mariadb-client curl python3.7 python3-dev python3-pip python3-venv python3.7-venv libapache2-mod-wsgi-py3 libapache2-mod-security2 libmariadb-dev-compat libmariadb-dev >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq install git ufw openssh-server apache2 libapache2-mod-php7.3 libsodium23 php php-common php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline psmisc php7.3-mbstring php7.3-zip php7.3-gd php7.3-xml php7.3-curl php7.3-mysql mariadb-server mariadb-client mysql-common curl python3.7 python3-dev python3-pip python3-venv python3.7-venv libapache2-mod-wsgi-py3 libapache2-mod-security2 libmariadb-dev-compat libmariadb-dev >/dev/null 2>&1";
 echo "${green}Fertig.${reset}";
 echo ""
 
