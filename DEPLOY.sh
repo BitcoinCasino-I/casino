91152684495b099685a7053ff31f17bc0fb25d78
Lars <lartschey@googlemail.com>
2021-01-12 17:35:31 +0100

    latest deploy script

--- DEPLOY.sh
+++ DEPLOY.sh
@@ -137,7 +137,7 @@ sudo -H -u "$APPUSER" bash -c "sudo rm -r /usr/share/phpmyadmin >/dev/null 2>&1"
 
 # Alle notwendigen Systempakete installieren
 echo "${yellow}Installiere alle nötigen Systempakete...${reset}";
-sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq install ufw openssh-server apache2 libapache2-mod-php7.3 libsodium23 php php-common php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline psmisc php7.3-mbstring php7.3-zip php7.3-gd php7.3-xml php7.3-curl php7.3-mysql mariadb-server mariadb-client curl python3 python3-dev python3-pip python3-virtualenv libapache2-mod-wsgi-py3 libapache2-mod-security2 libmariadb-dev-compat libmariadb-dev >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo apt-get -qq install git ufw openssh-server apache2 libapache2-mod-php7.3 libsodium23 php php-common php7.3 php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline psmisc php7.3-mbstring php7.3-zip php7.3-gd php7.3-xml php7.3-curl php7.3-mysql mariadb-server mariadb-client curl python3.7 python3-dev python3-pip python3-venv python3.7-venv libapache2-mod-wsgi-py3 libapache2-mod-security2 libmariadb-dev-compat libmariadb-dev >/dev/null 2>&1";
 echo "${green}Fertig.${reset}";
 echo ""
 
@@ -151,15 +151,22 @@ sudo -H -u "$APPUSER" bash -c "echo 'y' | sudo ufw enable >/dev/null";
 echo "${green}Fertig.${reset}";
 echo "";
 
-# Hole IP für Apache-Setup
-SERVERIP=$(curl -s ipinfo.io/ip);
-# TODO: Falls curl scheitert, alternative Methode nutzen
+# Lade Setup-Dateien herunter
+sudo -H -u "$APPUSER" bash -c "mkdir /home/$APPUSER/casinoapp-download";
+sudo -H -u "$APPUSER" bash -c "sudo git clone https://github.com/Lartsch/casinoapp-deploy-test.git /home/$APPUSER/casinoapp-download";
 
-# Beginne ApacheSetup
+# Beginne Apache-Setup
 echo "${yellow}Konfiguriere Apache...${reset}";
 sudo -H -u "$APPUSER" bash -c "sudo usermod -aG www-data $APPUSER";
 sudo -H -u "$APPUSER" bash -c "sudo chown www-data:www-data -R /var/www";
 sudo -H -u "$APPUSER" bash -c "sudo chmod 755 -R /var/www";
+sudo -H -u "$APPUSER" bash -c "sudo cp /home/$APPUSER/casinoapp-download/phpmyadmin.conf /etc/apache2/conf-available";
+sudo -H -u "$APPUSER" bash -c "sudo a2enconf phpmyadmin";
+sudo -H -u "$APPUSER" bash -c "sudo cp /home/$APPUSER/casinoapp-download/Casino.conf /etc/apache2/sites-available";
+sudo -H -u "$APPUSER" bash -c "sudo a2ensite Casino";
+sudo -H -u "$APPUSER" bash -c "sudo a2dissite 000-default";
+sudo -H -u "$APPUSER" bash -c "sudo rm /var/www/html/index.html";
+sudo -H -u "$APPUSER" bash -c "sudo mv /home/$APPUSER/casinoapp-download/CasinoApp /var/www/html";
 ENAPACHEMODULES="access_compat authz_user dir negotiation php7.3 reqtimeout status mpm_prefork alias autoindex env rewrite wsgi filter setenvif auth_basic cgid headers authn_core proxy socache_shmcb authn_file deflate mime ssl authz_core proxy_http authz_host";
 DISAPACHEMODULES="mpm_event";
 for VALDIS in $DISAPACHEMODULES; do
@@ -173,12 +180,11 @@ echo "${green}Fertig.${reset}";
 echo "";
 
 # Beginne MariaDB / phpMyAdmin Setup
-sudo -H -u "$APPUSER" bash -c "wget -q -P /home/$APPUSER/Downloads https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz";
+sudo -H -u "$APPUSER" bash -c "wget -q -P /home/$APPUSER/casinoapp-download https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz";
 sudo -H -u "$APPUSER" bash -c "sudo mkdir -p /usr/share/phpmyadmin";
-sudo -H -u "$APPUSER" bash -c "sudo tar xvf /home/$APPUSER/Downloads/phpMyAdmin-latest-all-languages.tar.gz --strip-components=1 -C /usr/share/phpmyadmin >/dev/null 2>&1";
-sudo -H -u "$APPUSER" bash -c "sudo cp ./config.inc.php /usr/share/phpmyadmin";
-sudo -H -u "$APPUSER" bash -c "sudo cp ./app-db.sql /usr/share/phpmyadmin/sql";
-sudo -H -u "$APPUSER" bash -c "sudo cp ./phpmyadmin.conf /etc/apache2/conf-available";
+sudo -H -u "$APPUSER" bash -c "sudo tar xvf /home/$APPUSER/casinoapp-download/phpMyAdmin-latest-all-languages.tar.gz --strip-components=1 -C /usr/share/phpmyadmin >/dev/null 2>&1";
+sudo -H -u "$APPUSER" bash -c "sudo cp /home/$APPUSER/casinoapp-download/config.inc.php /usr/share/phpmyadmin";
+sudo -H -u "$APPUSER" bash -c "sudo cp /home/$APPUSER/casinoapp-download/app-db.sql /usr/share/phpmyadmin/sql";
 sudo -H -u "$APPUSER" bash -c "sudo sed -i \"s/PHPUSERPW/$PHPUSERPW/g\" /usr/share/phpmyadmin/config.inc.php"
 sudo -H -u "$APPUSER" bash -c "sudo mkdir -p /var/lib/phpmyadmin/tmp";
 sudo -H -u "$APPUSER" bash -c "sudo chown -R www-data:www-data /var/lib/phpmyadmin";
@@ -191,15 +197,30 @@ sudo -H -u "$APPUSER" bash -c "sudo mariadb -e \"GRANT SELECT, INSERT, UPDATE, D
 sudo -H -u "$APPUSER" bash -c "sudo mariadb -e \"GRANT ALL PRIVILEGES ON *.* TO '$APPUSER'@'localhost' IDENTIFIED BY '$APPUSERPW' WITH GRANT OPTION;\"";
 DBUSER="${APPUSER}-appdb";
 sudo -H -u "$APPUSER" bash -c "sudo mariadb -e \"GRANT SELECT, INSERT, UPDATE, DELETE ON casinoapp.* TO '$DBUSER'@'localhost' IDENTIFIED BY '$DBUSERPW';\"";
-sudo -H -u "$APPUSER" bash -c "sudo a2enconf phpmyadmin";
 
 # Beginne mit App-Installation
-echo "${yellow}Installiere die Casino-Anwendung...${reset}";
-echo "${yellow}Lade Dateien...${reset}";
 echo "${yellow}Installiere die virtuelle Umgebung...${reset}";
+# Next line is needed as fix for opencv build failing. pip needs to be upgraded manually after initial installation
+python3 -m pip install --upgrade pip wheel setuptools opencv-python numpy
+# sudo not working here (permissions problem?) - SWITCH TO RUNSER GENERALLY OR ROOT-THEN-PERMISSIONS
+python3 -m venv /var/www/html/CasinoApp/venv;
+source /var/www/html/CasinoApp/venv/bin/activate;
+# Next two lines need to be installed seperately, build errors otherwise when installing requirements at oce
+python3 -m pip install wheel;
+python3 -m pip install -r /home/$APPUSER/casinoapp-download/requirements.txt;
+deactivate;
+echo "${yellow}Bearbeite Dateien...${reset}";
+sudo -H -u "$APPUSER" bash -c "sudo rm -r /home/$APPUSER/casinoapp-download";
+sudo -H -u "$APPUSER" bash -c "sudo sed -i \"s/'DBUSER'/'$DBUSER'/g\" /var/www/html/CasinoApp/db.cfg";
+sudo -H -u "$APPUSER" bash -c "sudo sed -i \"s/'DBUSERPW'/'$DBUSERPW'/g\" /var/www/html/CasinoApp/db.cfg";
+SERVERIP=$(curl -s ipinfo.io/ip);
+sudo -H -u "$APPUSER" bash -c "sudo sed -i \"s/APPDOMAIN = 'APPDOMAIN'/APPDOMAIN = '$SERVERIP'/g\" /var/www/html/CasinoApp/__init__.py";
 echo "${yellow}Setze Berechtigungen...${reset}";
+sudo -H -u "$APPUSER" bash -c "sudo chown -R www-data:www-data /var/www/html/CasinoApp";
+sudo -H -u "$APPUSER" bash -c "sudo chmod -R 750 /var/www/html/CasinoApp";
+sudo -H -u "$APPUSER" bash -c "sudo chmod -R 770 /var/www/html/CasinoApp/static/upload/profileimg";
 echo "${yellow}Starte Apache-Webserver neu...${reset}";
-# TODO use existing code in clone script
+sudo -H -u "$APPUSER" bash -c "sudo systemctl restart apache2";
 echo "${green}Fertig.${reset}";
 echo "";
 
